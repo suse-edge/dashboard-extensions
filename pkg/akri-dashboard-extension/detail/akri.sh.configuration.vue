2d2c08aa52cd76f2d1ce73fcc2efb5fcde342a84
Jiri Tomasek <jiri.tomasek@suse.com>
2023-10-13 10:38:40 +0200

    [Akri] Added configuration detail view

---
+++ pkg/akri-dashboard-extension/detail/akri.sh.configuration.vue
@@ -0,0 +1,29 @@
+<script>
+import { _CREATE } from '@shell/config/query-params';
+import ResourceTabs from '@shell/components/form/ResourceTabs';
+
+export default {
+  name: 'ConfigurationDetail',
+  components: { ResourceTabs },
+
+  props: {
+    mode: {
+      type: String,
+      default: _CREATE,
+    },
+
+    resource: {
+      type: String,
+      default: null,
+    },
+
+    value: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+<template>
+  <ResourceTabs mode="view" class="mt-20" :value="value" />
+</template>
