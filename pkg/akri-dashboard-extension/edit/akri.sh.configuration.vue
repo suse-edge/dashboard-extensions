a76b100e95583cccd07278cd535bd45a13304652
Jiri Tomasek <jiri.tomasek@suse.com>
2023-10-19 11:41:45 +0200

    [Akri] Add discovery handler logic

--- pkg/akri-dashboard-extension/edit/akri.sh.configuration.vue
+++ pkg/akri-dashboard-extension/edit/akri.sh.configuration.vue
@@ -4,25 +4,27 @@ import CruResource from '@shell/components/CruResource';
 import CreateEditView from '@shell/mixins/create-edit-view';
 // import FormValidation from '@shell/mixins/form-validation';
 import NameNsDescription from '@shell/components/form/NameNsDescription';
-import LabeledSelect from '@shell/components/form/LabeledSelect';
+// import LabeledSelect from '@shell/components/form/LabeledSelect';
 import Tab from '@shell/components/Tabbed/Tab';
 import Tabbed from '@shell/components/Tabbed';
 import { WORKLOAD_TYPES } from '@shell/config/types';
 import { set } from '@shell/utils/object';
 import UdevFields from '../components/UdevFields';
-
-// const initialDiscoveryHandlerSpec = {
-//   name: undefined,
-//   discoveryHandlerDetails: {},
-//   discoveryHandlerProperties: {},
-// };
-// const initialConfigurationSpec = {
-//   discoveryHandler: initialDiscoveryHandlerSpec,
-// };
+import { LabeledInput } from '@rancher/components';
+import YamlEditor from '@shell/components/YamlEditor';
 
 export default {
   name: 'CruConfiguration',
-  components: { CruResource, NameNsDescription, Tabbed, Tab, LabeledSelect, UdevFields },
+  components: {
+    CruResource,
+    NameNsDescription,
+    Tabbed,
+    Tab,
+    // LabeledSelect,
+    UdevFields,
+    LabeledInput,
+    YamlEditor,
+  },
   //   mixins: [CreateEditView, FormValidation],
   mixins: [CreateEditView],
   props: {
@@ -42,23 +44,6 @@ export default {
     },
   },
 
-  //   data() {
-  // this.$set(this.value, 'spec', this.value.spec || initialConfigurationSpec);
-  // return {
-  //   // v: this.value,
-  //   spec: this.value.spec || initialConfigurationSpec,
-  // };
-  // return {
-  //   discoveryHandlerName: 'hello',
-  // };
-  // this.$set(this.value, 'spec', this.value?.spec || {});
-  // this.$set(this.value, 'spec.discoveryHandler', this.value?.spec?.discoveryHandler || {});
-  // this.$set(
-  //   this.value,
-  //   'spec.discoveryHandler.name',
-  //   this.value?.spec?.discoveryHandler?.name || ''
-  // );
-  //   },
   data() {
     return {};
   },
@@ -76,21 +61,32 @@ export default {
         set(this.value, 'spec.discoveryHandler.name', newValue);
       },
     },
+    discoveryDetailsYaml: {
+      get() {
+        return this.value.spec?.discoveryHandler.discoveryDetails || '';
+      },
+      set(newValue) {
+        set(this.value, 'spec.discoveryHandler.discoveryDetails', newValue);
+      },
+    },
     isFormValid() {
       return !!this.value.name;
     },
     allDaemonSets() {
       return this.$store.getters['cluster/all'](WORKLOAD_TYPES.DAEMON_SET) || [];
     },
-    discoveryHandlers() {
-      return this.allDaemonSets.filter(
-        (daemonSet) => daemonSet.metadata.annotations['akri.sh.discoveryHandlerName']
-      );
-    },
     discoveryHandlerNames() {
-      return this.discoveryHandlers.map(
-        (dh) => dh.metadata.annotations['akri.sh.discoveryHandlerName']
-      );
+      return [
+        ...this.allDaemonSets
+          .filter((daemonSet) => daemonSet.metadata.annotations['akri.sh.discoveryHandlerName'])
+          .map((dh) => dh.metadata.annotations['akri.sh.discoveryHandlerName']),
+        'custom',
+      ];
+    },
+    discoveryHandlerTypes() {
+      return this.allDaemonSets
+        .filter((daemonSet) => daemonSet.metadata.annotations['akri.sh.discoveryHandlerType'])
+        .map((dh) => dh.metadata.annotations['akri.sh.discoveryHandlerType']);
     },
   },
 
@@ -131,17 +127,40 @@ export default {
         :label="t('akri.edit.configuration.tabs.discoveryHandler.title')"
         :weight="3"
       >
-        <div class="row mb-20">
+        <!-- <div class="row mb-20">
           <div class="col span-12">
             <LabeledSelect
               v-model="discoveryHandlerName"
-              :label="t('akri.edit.configuration.fields.discoveryHandlerType.label')"
+              :label="t('akri.edit.configuration.fields.discoveryHandlerName.label')"
               :options="discoveryHandlerNames"
               :mode="mode"
             />
           </div>
+        </div> -->
+        <div class="row mb-20">
+          <div class="col span-12">
+            <LabeledInput
+              v-model="discoveryHandlerName"
+              :label="t('akri.edit.configuration.fields.discoveryHandlerName.label')"
+            />
+          </div>
         </div>
         <UdevFields v-if="discoveryHandlerName === 'udev'" :mode="mode" :value="value" />
+        <template v-else>
+          <h3>
+            {{ t('akri.edit.configuration.fields.discoveryDetailsYaml.label') }}
+            <i
+              v-clean-tooltip="t('akri.edit.configuration.fields.discoveryDetailsYaml.tooltip')"
+              class="icon icon-info"
+            />
+          </h3>
+          <YamlEditor
+            ref="yamleditor"
+            v-model="discoveryDetailsYaml"
+            :mode="mode"
+            class="yaml-editor"
+          />
+        </template>
       </Tab>
       <Tab name="brokers" :label="t('akri.edit.configuration.tabs.brokers.title')" :weight="2">
         fields for broker pods and broker jobs go here
