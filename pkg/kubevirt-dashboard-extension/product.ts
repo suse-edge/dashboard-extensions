21fc566f2f37db995a07bbffcc28c46e0167d984
Jiri Tomasek <jiri.tomasek@suse.com>
2025-04-10 12:11:32 +0200

    [kubevirt] Additional virtualmachines product type config
    + Remove virtual machine instances page as the information
    is covered in the virtual machines list page

--- pkg/kubevirt-dashboard-extension/product.ts
+++ pkg/kubevirt-dashboard-extension/product.ts
@@ -1,20 +1,42 @@
 import { IPlugin } from '@shell/core/types';
-import {
-  KUBEVIRT_RESOURCE_NAME,
-  PRODUCT_NAME,
-  VMI_RESOURCE_NAME,
-  VM_RESOURCE_NAME,
-} from './constants';
+import { KUBEVIRT_RESOURCE_NAME, PRODUCT_NAME, VM_RESOURCE_NAME } from './constants';
 
 export function init(plugin: IPlugin, store: any) {
-  const { basicType, product } = plugin.DSL(store, PRODUCT_NAME);
+  const { basicType, product, configureType, weightType } = plugin.DSL(store, PRODUCT_NAME);
 
   product({
     inStore: 'cluster',
     showNamespaceFilter: true,
     ifHaveType: KUBEVIRT_RESOURCE_NAME,
+    to: {
+      name: `c-cluster-product-resource`,
+      params: {
+        product: PRODUCT_NAME,
+        resource: VM_RESOURCE_NAME,
+      },
+    },
   });
 
   // basicType([KUBEVIRT_RESOURCE_NAME, VM_RESOURCE_NAME, VMI_RESOURCE_NAME]);
-  basicType([VM_RESOURCE_NAME, VMI_RESOURCE_NAME]);
+  configureType(VM_RESOURCE_NAME, {
+    displayName: 'Virtual machines yo!',
+    isCreatable: true,
+    isEditable: true,
+    isRemovable: true,
+    showAge: true,
+    showState: true,
+    canYaml: true,
+    // customRoute: {
+    //   name: `c-cluster-${PRODUCT_NAME}-resource`,
+    //   params: {
+    //     product: PRODUCT_NAME,
+    //     resource: VM_RESOURCE_NAME,
+    //   },
+    // },
+  });
+
+  weightType(VM_RESOURCE_NAME, 2, true);
+  // weightType(VMI_RESOURCE_NAME, 1, true);
+
+  basicType([VM_RESOURCE_NAME]);
 }
